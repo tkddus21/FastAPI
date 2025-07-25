cd5416ff161a7dbbe5f2b9f315847629e126f772
tkddus21 <sangyeoni985@gmail.com>
2025-07-25 12:53:35 +0900

    whisper 기능 추가함.

--- sample/whisper.py
+++ sample/whisper.py
@@ -1,33 +1,9 @@
-from fastapi import FastAPI
-from starlette.middleware.cors import CORSMiddleware
-
-from domain.answer import answer_router
-from domain.question import question_router
+# main.py
 from fastapi import FastAPI, File, UploadFile
 import whisper
 import tempfile
 
 app = FastAPI()
-
-origins = [
-    "http://127.0.0.1:5173",    # 또는 "http://localhost:5173"
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-# @app.get("/hello")
-# def hello():
-#     return {"message": "안녕하세요 파이보!!212!"}
-
-app.include_router(question_router.router)
-app.include_router(answer_router.router)
-
 model = whisper.load_model("base")  # tiny, base, small, medium, large 중 선택
 
 @app.post("/transcribe/")
