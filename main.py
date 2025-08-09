7cfc691200e838eb07b6ddd6ae8ed65f782b3416
tkddus21 <sangyeoni985@gmail.com>
2025-08-09 19:56:28 +0900

    fastapi main파일 수정

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ app.include_router(answer_router.router)
 
 model = whisper.load_model("base")  # tiny, base, small, medium, large 중 선택
 
-@app.post("/transcribe/")
+@app.post("/transcribe")
 async def transcribe_audio(file: UploadFile = File(...)):
     # 업로드된 파일을 임시 파일로 저장
     with tempfile.NamedTemporaryFile(delete=False, suffix=".mp3") as tmp:
